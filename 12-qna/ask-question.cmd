825626a04784396696f857d83ca6a3d408b654ed
mustea93 <m.salihcavus@gmail.com>
2022-02-22 04:35:30 -0800

    qna

--- 12-qna/ask-question.cmd
+++ 12-qna/ask-question.cmd
@@ -2,7 +2,7 @@
 SETLOCAL ENABLEDELAYEDEXPANSION
 
 rem Set variables
-set prediction_url="YOUR_PREDICTION_ENDPOINT"
-set key="YOUR_KEY"
+set prediction_url="https://saloquestionanswering2.cognitiveservices.azure.com/language/:query-knowledgebases?projectName=LearnFAQ&api-version=2021-10-01&deploymentName=production"
+set key="d41e499bdfbe4eb2af0bec85a32a3f93"
 
 curl -X POST !prediction_url! -H "Ocp-Apim-Subscription-Key: !key!" -H "Content-Type: application/json" -d "{'question': 'What is a learning Path?' }"
